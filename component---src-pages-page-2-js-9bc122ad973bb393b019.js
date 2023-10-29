3be89bc6c80ab3bea6d46f33de39df3006a9a071
Samuel Sikes <sammaniamsam@gmail.com>
2023-10-29 18:06:18 -0500

    Manually updated gh-pages-branch

---
+++ component---src-pages-page-2-js-9bc122ad973bb393b019.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksikes_and_carlton=self.webpackChunksikes_and_carlton||[]).push([[617],{4948:function(e,t,n){n.r(t),n.d(t,{Head:function(){return r}});var a=n(7294),l=n(1883),c=n(9684),o=n(9357);const r=()=>a.createElement(o.Z,{title:"Page two"});t.default=()=>a.createElement(c.Z,null,a.createElement("h1",null,"Hi from the second page"),a.createElement("p",null,"Welcome to page 2"),a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-9bc122ad973bb393b019.js.map
--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
- */
-// gatsby-browser.js
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// You can delete this file if you're not using it
