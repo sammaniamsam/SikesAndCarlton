338263caf779f8c3f1753f8e7076802a810589f7
Samuel Sikes <sammaniamsam@gmail.com>
2023-10-29 19:45:08 -0500

    Added comments to component

--- src/components/withAlbumData.js
+++ src/components/withAlbumData.js
@@ -1,9 +1,14 @@
 // src/components/withAlbumData.js
+
+// Import necessary modules from React and Gatsby
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
+// Create a Higher Order Component (HOC) to fetch album data
 export const withAlbumData = (Component) => {
+  // This function receives a Component as its parameter
   return (props) => {
+    // Use Gatsby's useStaticQuery hook to perform a static GraphQL query.
     const data = useStaticQuery(graphql`
       query {
         allFile(
@@ -28,8 +33,12 @@ export const withAlbumData = (Component) => {
       }
     `);
 
-    // Filter the data based on the album prop
+    // Filter the fetched data based on the 'album' prop to retrieve data specific to the current album
+    // 'relativeDirectory' is assumed to represent an album's name or identifier
     const filteredData = data.allFile.edges.filter(edge => edge.node.relativeDirectory === props.album);
+
+    // Spread the original props and add the 'data' prop to the wrapped component
+    // The wrapped component now receives the filtered album data as a prop
     return <Component {...props} data={filteredData} />;
   };
 };
