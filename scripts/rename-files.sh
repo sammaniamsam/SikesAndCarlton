3771dfb615aebf1610ef5cd812ef40cf016c5d2c
Samuel Sikes <sammaniamsam@gmail.com>
2023-11-29 13:54:01 -0600

    Added script

---
+++ scripts/rename-files.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <parent_directory>"
+  exit 1
+fi
+
+parent_directory="$1"
+
+if [ ! -d "$parent_directory" ]; then
+  echo "Error: Parent directory not found."
+  exit 1
+fi
+
+# Find all JPEG files in subdirectories (excluding the starting directory)
+files=$(find "$parent_directory" -mindepth 2 -type f -iname "*.jpg" -o -iname "*.jpeg")
+
+# Loop through each file and rename based on DateTimeOriginal
+for file in $files; do
+  datetime=$(exiftool -Date_Time_Original -d "%b %e, %Y at %T" -s3 "$file")
+
+  if [ -z "$datetime" ]; then
+    # Try "DateTimeOriginal" if "Date_Time_Original" is not found
+    datetime=$(exiftool -DateTimeOriginal -d "%b %e, %Y at %T" -s3 "$file")
+  fi
+
+  if [ -n "$datetime" ]; then
+    # Convert the formatted date and time to a suitable format for a filename
+    datetime=$(date -j -f "%b %e, %Y at %T" "$datetime" "+%Y%m%d_%H%M%S")
+
+    # Extract file extension
+    extension="${file##*.}"
+
+    # Construct new filename
+    new_name="${datetime}.${extension}"
+
+    # Rename the file
+    mv "$file" "$new_name"
+
+    echo "Renamed: $file -> $new_name"
+  else
+    echo "Error: Unable to extract DateTimeOriginal from $file"
+    exiftool "$file"  # Add this line to print all Exif metadata for debugging
+  fi
+done
+
+echo "Renaming complete."
