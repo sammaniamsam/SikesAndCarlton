774916f83ed0745b7fb2c11719f7835bc8c2b31a
Samuel Sikes <sammaniamsam@gmail.com>
2023-12-07 15:45:16 -0600

    Added custom prop for max width

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
 import * as React from "react";
-
 import "../css/layout.css";
 import NavBar from "./nav";
 import IconGroup from "./iconGroup";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, maxWidthOverride }) => {
+  const customMaxWidth = maxWidthOverride || "var(--size-content)";
+
   return (
     <>
       <NavBar />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
+          maxWidth: customMaxWidth,
           padding: `var(--size-gutter)`,
         }}
       >
