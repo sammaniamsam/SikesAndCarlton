52669dfbe760abc7b1b29a3b105a9864e042f4b0
Samuel Sikes <sammaniamsam@gmail.com>
2023-12-09 13:16:04 -0600

    Added code to sort albums by their content

--- src/hooks/useAlbums.js
+++ src/hooks/useAlbums.js
@@ -9,6 +9,7 @@ export const useAlbums = () => {
         edges {
           node {
             relativeDirectory
+            publicURL
           }
         }
       }
@@ -18,20 +19,44 @@ export const useAlbums = () => {
   const albums = Array.from(
     new Set(data.allFile.edges.map(({ node }) => node.relativeDirectory))
   )
-    .map((directory) => ({
-      title: directory
-        .split("/")
-        .map((subDirectory) => {
-          return subDirectory
-            .split("-")
-            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(" ");
-        })
-        .join(" / "),
-      path: directory,
-    }))
+    .map((directory) => {
+      const photos = data.allFile.edges.filter(
+        (edge) => edge.node.relativeDirectory === directory
+      );
+
+      // Extract the names from the publicURLs
+      const photoNames = photos.map((photo) =>
+        photo.node.publicURL
+          .split("/")
+          .pop()
+          .split("_")
+          .join("")
+          .replace(/\.jpg$/, "")
+      );
+
+      return {
+        title: directory
+          .split("/")
+          .map((subDirectory) => {
+            return subDirectory
+              .split("-")
+              .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(" ");
+          })
+          .join(" / "),
+        path: directory,
+        photoNames: photoNames,
+      };
+    })
     .filter((album) => album.title.length > 0 && album.path.length > 0)
-    .sort((a, b) => a.title.localeCompare(b.title));
+    .sort((a, b) => {
+      const getTimestampFromName = (name) => (isNaN(name) ? 0 : name);
+
+      const timestampA = Math.max(...a.photoNames.map(getTimestampFromName));
+      const timestampB = Math.max(...b.photoNames.map(getTimestampFromName));
+
+      return timestampB - timestampA;
+    });
 
   return albums;
 };
