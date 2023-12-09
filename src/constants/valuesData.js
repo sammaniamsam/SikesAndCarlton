e07243919716a1b5b7f596e27492ccbf4b915b7a
Samuel Sikes <sammaniamsam@gmail.com>
2023-12-09 09:46:55 -0600

    Added about page content

---
+++ src/constants/valuesData.js
@@ -0,0 +1,19 @@
+const values = [
+  {
+    title: "Trust",
+    content:
+      "Trust is the bedrock of our relationships. Our promise is backed by a 90-day warranty on all plant services and an extended 180-day warranty for drainage solutions and structures, demonstrating our unwavering commitment to earning and preserving your trust.",
+  },
+  {
+    title: "Quality",
+    content:
+      "Quality is not just a commitment; it's our essence. We pledge to consistently deliver products that not only meet but exceed expectations, setting a benchmark for excellence.",
+  },
+  {
+    title: "Excellence from Within",
+    content:
+      "The foundation is the most crucial aspect of any project. That’s why our cornerstone is our team and partners. It is through the talents and dedication of our team that we achieve excellence.",
+  },
+];
+
+export default values;
