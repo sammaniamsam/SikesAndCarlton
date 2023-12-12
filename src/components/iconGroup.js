b453bb95e7295e9dca423d71019fb3b8a0e7a84b
Samuel Sikes <sammaniamsam@gmail.com>
2023-12-12 15:07:07 -0600

    Added icon and link to FB

--- src/components/iconGroup.js
+++ src/components/iconGroup.js
@@ -1,38 +1,46 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faYoutube, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube, faGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import "../css/iconGroup.css"; // Ensure this file is in the same directory and contains the previous CSS
 
 const IconGroup = () => {
   return (
-    <Container fluid>
-      <Row className="justify-content-center">
-        <Col xs={12} md={6} className="text-center">
-          <div className="icon-group">
-            <div className="icon-spacing">
-              <a
-                style={{ paddingRight: 5 }}
-                href="https://maps.app.goo.gl/wMzFithEsGXttbPz5"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="icon-button"
-              >
-                <FontAwesomeIcon icon={faGoogle} size="2x" />
-              </a>
-              <a
-                href="https://www.youtube.com/channel/UCF9XepynEnqC1kGc-0s6w-w"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="icon-button"
-              >
-                <FontAwesomeIcon icon={faYoutube} size="2x" />
-              </a>
+      <Container fluid>
+        <Row className="justify-content-center">
+          <Col xs={12} md={6} className="text-center">
+            <div className="icon-group">
+              <div className="icon-spacing">
+                <a
+                    href="https://maps.app.goo.gl/wMzFithEsGXttbPz5"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="icon-button"
+                >
+                  <FontAwesomeIcon icon={faGoogle} size="2x" />
+                </a>
+                <a
+                    style={{ paddingRight: 5, paddingLeft: 5 }}
+                    href="https://www.youtube.com/channel/UCF9XepynEnqC1kGc-0s6w-w"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="icon-button"
+                >
+                  <FontAwesomeIcon icon={faYoutube} size="2x" />
+                </a>
+                <a
+                    href="https://www.facebook.com/people/Sikes-Carlton-Corporation/61554503313733/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="icon-button"
+                >
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+              </div>
             </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Row>
+      </Container>
   );
 };
 
